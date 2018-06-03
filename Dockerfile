689eeb295051a074a5c4e87b4686c7e353772d1c
Al Nemec <al.nemec@gmail.com>
2018-06-02 21:19:34 -0500

    added dist directory and test dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# this same shows how we can extend/change an existing official image from Docker Hub
+
+FROM nginx:latest
+# highly recommend you always pin versions for anything beyond dev/learn
+
+WORKDIR /usr/share/nginx/html
+# change working directory to root of nginx webhost
+# using WORKDIR is prefered to using 'RUN cd /some/path'
+RUN apt-get update && apt-get install -y git && apt-get install -y vim
+
+RUN git clone https://github.com/althe3rd/UW-Vue-Proj.git && rm -rf index.html && cp -R UW-Vue-Proj/dist/. ./
+#RUN ls -la /*
+
+#COPY ./UW-Vue-Proj ./
+# I don't have to specify EXPOSE or CMD because they're in my FROM
---
+++ dist/precache-manifest.7334aa8f450903320cf627dcc70cfa63.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "56d7095f1a6c9b2124e79a1e398af742",
+    "url": "/fonts/VerlagBlack.56d7095f.woff2"
+  },
+  {
+    "revision": "cca1581308a50cc0f80e",
+    "url": "/js/vendors~app.cca15813.js"
+  },
+  {
+    "revision": "3130afa9db9501631cff23fceb4fb3ba",
+    "url": "/fonts/VerlagBlack.3130afa9.woff"
+  },
+  {
+    "revision": "e8ae7ad471a76102a33d",
+    "url": "/js/app.e8ae7ad4.js"
+  },
+  {
+    "revision": "aae32f996fe01c1d994552b267d241ab",
+    "url": "/fonts/VerlagBook.aae32f99.eot"
+  },
+  {
+    "revision": "54d4aff5106b8a88ae89257f9b37ac66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a340a42fce3c51a8423c7b5f9a049a",
+    "url": "/fonts/VerlagBook.a5a340a4.woff"
+  },
+  {
+    "revision": "90bdc158036b4a23417f480185bffa2b",
+    "url": "/fonts/VerlagBook.90bdc158.woff2"
+  },
+  {
+    "revision": "9af7a698b5a57c17d2ebd5b5e3781019",
+    "url": "/fonts/VerlagBook.9af7a698.ttf"
+  },
+  {
+    "revision": "396f59bb5b01777fb5dcbeb6fd344b36",
+    "url": "/img/uw_logo.396f59bb.svg"
+  },
+  {
+    "revision": "42e601fe2972a0b346b2adc5f18e1f49",
+    "url": "/img/uw_logo_dark.42e601fe.svg"
+  },
+  {
+    "revision": "c05f97fc8f519e52cf1557af8c1c5b27",
+    "url": "/img/VerlagBlack.c05f97fc.svg"
+  },
+  {
+    "revision": "e7c5342909f8b550c76ed4be956f5fa7",
+    "url": "/img/Verlag-Light.e7c53429.svg"
+  },
+  {
+    "revision": "85fe9bc3a58ad2cf6dcf5e4ba92c48e9",
+    "url": "/img/VerlagBook.85fe9bc3.svg"
+  },
+  {
+    "revision": "cebdfd2d87769f82aaed0ed74b3760b8",
+    "url": "/fonts/VerlagBlack.cebdfd2d.eot"
+  },
+  {
+    "revision": "62604cffa7ae30c754ea2520e20c48db",
+    "url": "/fonts/VerlagBlack.62604cff.ttf"
+  },
+  {
+    "revision": "fb214c19d8cd9507a02f18851f80d2f1",
+    "url": "/fonts/Verlag-Light.fb214c19.woff"
+  },
+  {
+    "revision": "b960e6b042e46648ac7d1203f5a7c0df",
+    "url": "/fonts/Verlag-Light.b960e6b0.eot"
+  },
+  {
+    "revision": "0790e999411a38893cd7acd73b5f9ecc",
+    "url": "/fonts/Verlag-Light.0790e999.ttf"
+  },
+  {
+    "revision": "e8ae7ad471a76102a33d",
+    "url": "/css/app.f3ff507c.css"
+  }
+];
